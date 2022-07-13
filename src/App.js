01f91d1f19b0ae79dec85c0787e16790c42bdac3
metulboss <metulromel@gmail.com>
2022-07-14 00:57:56 +0600

    nasted mapping done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const users = [
+
+    {
+      fullName:'Metul Romel',
+      age : 23,
+
+      phone : [
+      {home:'01304284953'},
+      {office :'92301133'}
+      ]
+      
+    },
+    {
+      fullName:'Zahurul Islam',
+      age : 20,
+
+      phone : [
+      {home:'01304284953'},
+      {office :'92301133'}
+      ]
+      
+    }
+
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className='heading'>nested-mapping</h1>
+
+      {
+        users.map((user,index) => <article key={index}>
+          <h1>FullName:{user.fullName}</h1>
+          <p> Age:{user.age}</p>
+          {
+            user.phone.map((phone,index)=> <div key={index}>
+              <p> Home:{phone.home}</p>
+              <p> Office: {phone.office}</p>
+            </div>)
+          }
+        </article>)
+      }
+
     </div>
   );
 }
